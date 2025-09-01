242612c7cdc9242cb0986fb40caa9c5b46f6e770
Загарий Дмитрий Сергеевич (zagariy_ds) <zagariy_ds@nlmk.com>
2025-09-01 17:03:37 +0300

    fix

---
+++ backend/demo/src/main/java/com/example/demo/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull; // добавляем нужный импорт
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://hram48plosk.ru") // Добавь сюда нужный источник
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // Если нужно поддерживать авторизацию (куки и т.д.)
+    }
+}
