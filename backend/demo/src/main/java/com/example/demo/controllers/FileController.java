91a891ccd07e8f8465ec4e565e45072db897d507
Загарий Дмитрий Сергеевич (zagariy_ds) <zagariy_ds@nlmk.com>
2025-09-12 12:39:26 +0300

    пересобери

--- backend/demo/src/main/java/com/example/demo/controllers/FileController.java
+++ backend/demo/src/main/java/com/example/demo/controllers/FileController.java
@@ -4,7 +4,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
