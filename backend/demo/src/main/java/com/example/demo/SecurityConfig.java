c5ea4f4c81ed9e68810921a8f04ab64e7a40b15e
Загарий Дмитрий Сергеевич (zagariy_ds) <zagariy_ds@nlmk.com>
2025-09-23 11:15:38 +0300

    fix it

--- backend/demo/src/main/java/com/example/demo/SecurityConfig.java
+++ backend/demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.services.*;
+import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 public class SecurityConfig {
+
     @Autowired
     JwtFilter jwtFilter;
 
@@ -17,10 +19,13 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/news", "/api/schedule").permitAll() // Разрешаем
+                                                                                                   // GET-запросы
                         .requestMatchers("/api/login", "/api/public/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterAfter(jwtFilter,
                         org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
