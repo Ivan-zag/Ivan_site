8c443b50d003480b111bd080aec24bbaf00e5742
Загарий Дмитрий Сергеевич (zagariy_ds) <zagariy_ds@nlmk.com>
2025-09-25 13:22:02 +0300

    fix

--- backend/demo/src/main/java/com/example/demo/SecurityConfig.java
+++ backend/demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -14,6 +16,11 @@ public class SecurityConfig {
 
     @Autowired
     JwtFilter jwtFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
