81da75e5a19cc5be262334f6c904e898a1884db5
Загарий Дмитрий Сергеевич (zagariy_ds) <zagariy_ds@nlmk.com>
2025-09-26 14:25:07 +0300

    fix2

--- backend/demo/src/main/java/com/example/demo/SecurityConfig.java
+++ backend/demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -36,10 +36,6 @@ public class SecurityConfig {
                         // Доступ для всех к GET запросам на /api/news и /api/schedule/last
                         .requestMatchers(HttpMethod.GET, "/api/news", "/api/schedule/last").permitAll()
                         .requestMatchers("/api/login", "/api/register", "/api/public/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/news").hasAuthority("ADMIN") // Только админы
-                        .requestMatchers(HttpMethod.PUT, "/api/news").hasAuthority("ADMIN") // Только админы
-                        .requestMatchers(HttpMethod.DELETE, "/api/news").hasAuthority("ADMIN") // Только админы
-                        .requestMatchers("/api/schedule/upload").hasRole("ADMIN")
                         .anyRequest().authenticated());
         http.addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
